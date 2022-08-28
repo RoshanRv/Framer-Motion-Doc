0991911ae88b3dd7e190b3b9bb3fedfc63640a25
Roshan <nkroshankumar@gmail.com>
2022-08-28 20:45:43 +0530

    removed unused modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,10 @@
-import { motion ,AnimatePresence} from "framer-motion"
+import { AnimatePresence} from "framer-motion"
 import {Routes,Route, useLocation} from 'react-router-dom'
-import {useState} from 'react'
 import Home from "./pages/Home"
 import Parallax from "./pages/Parallax"
 import Footer from "./components/Footer"
 
-const divVariant={
-  hidden:{
-    x:'-100vw', 
-  },
-  visible:{
-    x:0
-
-    ,transition:{
-      delay:.1,duration:.4,type:'spring'
-    }
-  },
-  exit:{
-    x:'-100vw'
-  }
-}
-
-const hello={
-  hidden:{
-  },
-  visible:{
-    scale:1.5,
-    // originX:0,
-    
-    transition:{
-      delay:.6,
-      yoyo:Infinity
-    }
-  }
-}
-
 function App() {
-
   
   const location = useLocation()
 
